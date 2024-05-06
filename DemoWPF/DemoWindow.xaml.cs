61daecbc97984bb0aa8389b9039e0d6fd0d0a7c2
tungpthe171795 <tungpthe171795@gmail.com>
2024-05-06 14:24:17 +0700

    Add project files.

---
+++ DemoWPF/DemoWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DemoWPF
+{
+    /// <summary>
+    /// Interaction logic for DemoWindow.xaml
+    /// </summary>
+    public partial class DemoWindow : Window
+    {
+        public DemoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToPage01_Click(object sender, RoutedEventArgs e)
+        {
+            btnToPage01.Content = "Demo";
+            frMain.Content = new Page_01();
+        }
+        private void btnToPage02_Click(object sender, RoutedEventArgs e)
+        {
+            frMain.Content = new Page_02();
+        }
+    }
+}
