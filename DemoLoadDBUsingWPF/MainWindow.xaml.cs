96d57f3f6d0e10b2b38313efe2267596609544b8
tungpthe171795 <tungpthe171795@gmail.com>
2024-05-15 12:49:23 +0700

    Add LoadDbDemo

---
+++ DemoLoadDBUsingWPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DemoLoadDBUsingWPF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoLoadDBUsingWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            Load();
+        }
+
+        private void Load()
+        {
+            var list = PRN211_1Context.INSTANCE.Students.Include(x => x.Depart).ToList();
+            dgvDisplay.ItemsSource = list;
+            var cbxDepartContent = PRN211_1Context.INSTANCE.Departments.ToList();
+            cbxDepart.ItemsSource = cbxDepartContent;
+
+        }
+    }
+}
