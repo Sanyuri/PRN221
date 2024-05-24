e293d2d40a50464ff4a34377ce59175eee99cb7c
tungpthe171795 <tungpthe171795@gmail.com>
2024-05-24 17:44:39 +0700

    Add demo HTTPClient

---
+++ DemoHttpClient/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoHttpClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        readonly HttpClient client = new HttpClient();
+        private void btnClose_Click(object sender, RoutedEventArgs e) => Close();
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtContent.Text = string.Empty;
+        }
+        private async void btnViewHTML_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = txtURL.Text;
+            //Call asynchronous network methods
+            //in try/catch block to handle exception
+            try
+            {
+                string responseBody = await client.GetStringAsync(uri);
+                txtContent.Text = responseBody.Trim();
+            }catch (Exception ex)
+            {
+                MessageBox.Show($"Message: {ex.Message}");
+            }
+        }
+    }
+}
