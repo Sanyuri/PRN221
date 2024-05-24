12513acdc83b37c5f951a01a5b04c8c85b4012f0
tungpthe171795 <tungpthe171795@gmail.com>
2024-05-24 17:44:54 +0700

    add DemoDNS

---
+++ DemoDNS/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//DNS
+using System.Net;
+
+Console.WriteLine(new string('*', 50));
+var domainEntry = Dns.GetHostEntry("www.fpt.edu.vn");
+Console.WriteLine("Name: " + domainEntry.HostName);
+foreach (var ip in domainEntry.AddressList)
+{
+    Console.WriteLine(ip);
+}
+Console.WriteLine(new string('*',30));
+domainEntry = Dns.GetHostEntry("127.0.0.1");
+Console.WriteLine("Name: " + domainEntry.HostName);
+foreach (var ip in domainEntry.AddressList)
+{
+    Console.WriteLine(ip);
+}
+Console.WriteLine();
+
